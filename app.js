58bd2d77bc729464c33b9f49efadee76f387771e
s961744 <A0012272@cppcb.com.tw>
2018-02-01 08:54:02 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ app.post('/',function(req,res){
     wechatApp.handleMsg(req,res);
 });
 
+// 維持Heroku不Sleep
+setInterval(function () {
+    http.get("http://cpcwechatbot.herokuapp.com");
+    var msg_data = JSON.parse(JSON.stringify('{"touser": "' + process.env.adminId + '", "msgtype": "text", "agentid": ' + process.env.agentId + ', "text" : { "content": "APP alive" }, "safe": 0}'));
+    token.getAccessToken("agent", process.env.agentSecret).then(function (data) {
+        msg.postMsg(data, msg_data);
+    });
+}, 1500000); // every 25 minutes (1500000)
+
 // 因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 443, function () {
     var port = server.address().port;
@@ -111,11 +120,16 @@ var job = schedule.scheduleJob('0 0,10,20,30,40,50 * * * *', function ()
                         {
                             user.createUser(data, user_info).then(function (result)
                             {
+                                var result = JSON.parse(data);
                                 if (result.errcode == "0") {
                                     var userAuth = JSON.parse(JSON.stringify('{"user_id": "' + user_id + '", "status": "ES"}'));
                                     user.updateUserStatus(userAuth).then(function (data) {
                                         console.log("Create user success:" + user_id);
                                     });
+                                }
+                                else
+                                {
+                                    console.log("Create user error, user_id = " + user_id + ",errcode=" + result.errcode);
                                 }
                             });
                         });
